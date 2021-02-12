5c61fd794af40dc0d85237f5a15e4bb474d4c8a1
mayevskyivan5@gmail.com <mayevskyivan5@gmail.com>
2021-02-12 19:27:57 +0200

    connection to the beaver

--- DbUsers/Entities/EFContext.cs
+++ DbUsers/Entities/EFContext.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DbUsers.Entities
 {
-    class EFContext
+    public class EFContext : DbContext
     {
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=91.238.103.51;Port=5743;Database=mayevskydb;Username=maey;Password=$544$B77w**G)K$t!Ube22}mav");
+        }
     }
 }
